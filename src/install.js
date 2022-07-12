be707a7e433a8257345b028a858a826c8a652783
Zonk Dev <zonkdev@yahoo.com>
2022-07-12 12:03:34 +0700

    setup project

--- src/install.js
+++ src/install.js
@@ -2,8 +2,9 @@ import NButton from "@/components/NButton.vue";
 import NInput from "@/components/NInput.vue";
 import NInputGroup from "@/components/NInputGroup.vue";
 import NIcon from "@/components/NIcon.vue";
-import NFormAction from "@/components/NForm/NFormAction.vue";
+import NFileUpload from "@/components/NFileUpload.vue";
 import NForm from "@/components/NForm/NForm.vue";
+import NFormAction from "@/components/NForm/NFormAction.vue";
 import NFormSection from "@/components/NForm/NFormSection.vue";
 import NFormInput from "@/components/NForm/NFormInput.vue";
 
@@ -13,6 +14,7 @@ const HexaUi = {
     Vue.component("NInput", NInput);
     Vue.component("NInputGroup", NInputGroup);
     Vue.component("NIcon", NIcon);
+    Vue.component("NFileUpload", NFileUpload);
     Vue.component("NForm", NForm);
     Vue.component("NFormAction", NFormAction);
     Vue.component("NFormSection", NFormSection);
